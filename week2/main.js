903fc2df8a6e98a1515a002e118219f0644438ea
yadajen <110714179+yadajen@users.noreply.github.com>
2023-12-14 05:01:35 +0700

    Update main.js

--- week2/main.js
+++ week2/main.js
@@ -25,8 +25,9 @@ let total
 console.log(total)
 if(total === undefined) console.log('varible does not have initial value')
 if(a===null) console.log('variable has null value')
-if('2'==2) console.log(`'2'==2' : ${'2' == 2}`)
-if('2'===2) console.log(`'2'===2' : ${'2' === 2}`) //'2'==2' : true
+if('2'==2) console.log(`'2'==2' : ${'2' == 2}`) //'2'==2' : true
+if('2'===2) console.log(`'2'===2' : ${'2' === 2}`) //'2'===2' : false
+
 
 //object sample
 const obj={id:1001, name: 'pen',price:100}
